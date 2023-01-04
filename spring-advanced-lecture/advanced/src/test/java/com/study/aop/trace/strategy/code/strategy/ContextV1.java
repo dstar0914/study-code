a1dc6aa9578825e6292e8d0e6bdc972c26acabeb
dstar0914 <demon0914@naver.com>
2023-01-05 00:10:37 +0900

    [spring-advanced-lecture] 전략 패턴 적용

---
+++ spring-advanced-lecture/advanced/src/test/java/com/study/aop/trace/strategy/code/strategy/ContextV1.java
@@ -0,0 +1,29 @@
+package com.study.aop.trace.strategy.code.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 필드에 전략을 보관하는 방식
+ */
+@Slf4j
+public class ContextV1 {
+
+    private Strategy strategy;
+
+    public ContextV1(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void execute() {
+        long startTime = System.currentTimeMillis();
+
+        // 위임.
+        strategy.call();
+
+        long endTime = System.currentTimeMillis();
+
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+}
